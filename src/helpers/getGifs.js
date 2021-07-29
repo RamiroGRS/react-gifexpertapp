1c8d1c275cf61d71793cbdee07ace6fa4e380a9a
Ramiro Resendiz <018027552@upq.edu.mx>
2021-07-29 01:23:02 -0500

    getGifs.js actualizado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=CBwyTaXiI8x4N14451NsdNE5p1A1kT7i&q=${ encodeURI(category) }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=CBwyTaXiI8x4N14451NsdNE5p1A1kT7i&q=${ encodeURI(category) }&limit=15`;
     const resp = await fetch ( url );
     const { data } = await resp.json();
     const gifs = data.map( img => {
